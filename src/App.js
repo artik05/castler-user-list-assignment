2790881e55a3fdac074ea967611797b44633d887
artik05 <artijha05@gmail.com>
2022-09-26 23:14:54 +0530

    fixed mb view

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ export default function App() {
   const [userId, setUserId] = useState(0);
 
   return (
-    <div className="App container-fluid w-50 p-5">
+    <div className="App container-fluid">
       <div className="row m-1">
-        <div className="col-md-12">
+        <div className="col-md-12 col-xs-12">
           <form>
             <input type="number" className="form-control" placeholder="Enter User Number i.e. 1,2,3" onInput={(e) => setUserId(e.target.value)} />
           </form>
